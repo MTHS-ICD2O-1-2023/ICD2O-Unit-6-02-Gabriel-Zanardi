d898afe77f0e75f48d281a70ab18672fb3c253ea
Gabriel Zanardi Nogueira <159185296+Gabriel-Zanardi@users.noreply.github.com>
2024-05-16 18:06:11 +0000

    init html and js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: May  2024
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  positiveChecked = document.getElementById('positive-number').checked
+  negativeChecked = document.getElementById('negative-number').checked
+
+const randomNumber = Math.floor(Math.random() * 99) + 1
+
+  if (positiveChecked == true) {
+    document.getElementById("number-answer").innerHTML = randomNumber
+  } else {
+    document.getElementById("number-answer").innerHTML = (randomNumber * -1)
+  }
+}
