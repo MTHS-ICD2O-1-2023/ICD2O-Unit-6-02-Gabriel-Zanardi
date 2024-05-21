9a025bf55254ef51b3e267c48875ea6c17c88324
Gabriel Zanardi Nogueira <159185296+Gabriel-Zanardi@users.noreply.github.com>
2024-05-21 01:55:24 +0000

    formatted the js

--- js/script.js
+++ js/script.js
@@ -3,21 +3,22 @@
 // Created by: May  2024
 // This file contains the JS functions for index.html
 
-    "use strict"
+  "use strict"
 
-    let clicks = parseInt(localStorage.getItem("click-counter")) || 0
+  let clicks = parseInt(localStorage.getItem("click-counter")) || 0
 
-    function updateCookieCount() {
+  function updateCookieCount() {
 
-        document.getElementById("click-count").innerHTML = clicks
-    }
+    document.getElementById("click-count").innerHTML = clicks
+  }
 
-    function myButtonClicked() {
-        clicks += 1
-        
-        // Update the HTML element with the updated clicks value
-        document.getElementById("click-count").innerHTML = clicks
+  function myButtonClicked() {
+    clicks += 1
 
-        // Update the clicks value in local storage
-        localStorage.setItem("click-counter", clicks)
-    }
+    // Update the HTML element with the updated clicks value
+    document.getElementById("click-count").innerHTML = clicks
+
+    // Update the clicks value in local storage
+    localStorage.setItem("click-counter", clicks)
+  }
+  
